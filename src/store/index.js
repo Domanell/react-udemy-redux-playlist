736010d7fb69b806fd5639a87306e435dd4fdb50
Oleksandr Pavlii <pavliy.av@gmail.com>
2024-10-06 17:35:39 +0200

    Add movies slice and actions

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 // Slice creation (piece of state of store)
 const songsSlice = createSlice({
-	name: 'song',
+	name: 'song', // uses as an action identifier
 	initialState: [],
 	reducers: {
 		// 'song' + '/' + 'addSong'
@@ -16,12 +16,28 @@ const songsSlice = createSlice({
 	},
 });
 
+const movieSlice = createSlice({
+	name: 'movie',
+	initialState: [],
+	reducers: {
+		addMovie(state, action) {
+			state.push(action.payload);
+		},
+		removeMovie(state, action) {
+			const index = state.indexOf(action.payload);
+			state.splice(index, 1);
+		},
+	},
+});
+
 // Store creation (from slices)
 const store = configureStore({
 	reducer: {
 		songs: songsSlice.reducer, // store key (custom storage state name) and value (reducer that handle state, uses init value)
+		movies: movieSlice.reducer,
 	},
 });
 
 export { store };
 export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = movieSlice.actions;
